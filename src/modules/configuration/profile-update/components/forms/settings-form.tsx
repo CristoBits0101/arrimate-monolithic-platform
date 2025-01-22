29c877e8bec7d3a32c9d5602fe1a3138263c367b
Cristo Suárez <cristobits0101@gmail.com>
2025-01-22 11:46:20 +0000

    Refactoring profile form

--- src/modules/configuration/profile-update/components/forms/settings-form.tsx
+++ src/modules/configuration/profile-update/components/forms/settings-form.tsx
@@ -1,144 +1,104 @@
 'use client'
 
-// Alerts
-import FormError from '@/modules/auth/form-pieces/alerts/error-alert'
-import FormSuccess from '@/modules/auth/form-pieces/alerts/success-alert'
-
-// Buttons
-import SubmitButton from '@/modules/configuration/profile-update/components/buttons/submit-button'
-
 // Cards
 import CardWrapper from '@/modules/configuration/profile-update/components/cards/card-wrapper'
-
-// Customized
-import { useSettingsForm } from '@/modules/configuration/profile-update/hooks/useSettingsForm'
 
 // Fieldset
 import FormFieldset from '@/modules/configuration/profile-update/components/fieldsets/form-fieldset'
 
 // Forms
-import { FormProvider } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 
-// Google
-import autocomplete from '@/lib/google/google-maps'
-import { PlaceAutocompleteResult } from '@googlemaps/google-maps-services-js'
-
-// Inputs
-import AddressInput from '@/modules/configuration/profile-update/components/inputs/address-input'
-import BirthdateInput from '@/modules/configuration/profile-update/components/inputs/birthdate-input'
-import CityInput from '@/modules/configuration/profile-update/components/inputs/city-input'
-import CountryInput from '@/modules/configuration/profile-update/components/inputs/country-input'
-import EmailInput from '@/modules/configuration/profile-update/components/inputs/email-input'
-import GenderInput from '@/modules/configuration/profile-update/components/inputs/gender-input'
-import InterestsInput from '@/modules/configuration/profile-update/components/inputs/interests-input'
-import NameInput from '@/modules/configuration/profile-update/components/inputs/name-input'
-import NewPasswordInput from '@/modules/configuration/profile-update/components/inputs/new-password-input'
-import NicknameInput from '@/modules/configuration/profile-update/components/inputs/nickname-input'
-import OccupationInput from '@/modules/configuration/profile-update/components/inputs/occupation-input'
-import PasswordInput from '@/modules/configuration/profile-update/components/inputs/password-input'
-import PhoneNumberInput from '@/modules/configuration/profile-update/components/inputs/phone-number-input'
-import PhonePrefixInput from '@/modules/configuration/profile-update/components/inputs/phone-prefix-input'
-import PortfolioInput from '@/modules/configuration/profile-update/components/inputs/portfolio-input'
-import SloganInput from '@/modules/configuration/profile-update/components/inputs/slogan-input'
-import ZipCodeInput from '@/modules/configuration/profile-update/components/inputs/zip-code-input'
-
-// Intl
+// Translations
 import { useTranslations } from 'next-intl'
 
-// React
-import { useEffect, useState } from 'react'
-
-// Shadcn
-import { Form } from '@/modules/ui/form'
-
 export default function SettingsForm() {
+  // Forms
+  const { handleSubmit, register } = useForm()
+
+  // Handlers
+  const onSubmit = handleSubmit((data) => {
+    console.log(data)
+  })
+
   // Translations
-  const f = useTranslations('ProfileForm')
-  const t = useTranslations('Button')
+  const pf = useTranslations('ProfileForm')
+  const f = useTranslations('Forms')
 
-  // Custom hook
-  const { form, error, success, isPending, hydrated, onSubmit } = useSettingsForm()
-
-  // Predictions state
-  const [predictions, setPredictions] = useState<PlaceAutocompleteResult[]>([])
-
-  // Confirmation state
-  const [country, setCountry] = useState<string>('')
-  const [city, setCity] = useState<string>('')
-
-  // Value predictions
-  const [value, setValue] = useState<string>('')
-
-  // Fetch predictions useEffect
-  useEffect(() => {
-    const fetchPredictions = async () => {
-      try {
-        const results = await autocomplete(value)
-        setPredictions(results)
-      } catch (error) {
-        console.error('Error fetching predictions: ', error)
-      }
-    }
-    fetchPredictions()
-  }, [value])
-
-  return hydrated ? (
+  return (
     <CardWrapper>
-      <FormProvider {...form}>
-        <Form {...form}>
-          <form className='space-y-8' onSubmit={form.handleSubmit(onSubmit)}>
-            <div className='space-y-8 mt-8'>
-              {/* Identity */}
-              <FormFieldset legend={f('identity')}>
-                <NameInput name='name' isPending={isPending} />
-                <NicknameInput name='nickname' isPending={isPending} />
-                <GenderInput name='gender' isPending={isPending} />
-                <BirthdateInput name='birthdate' isPending={isPending} />
-              </FormFieldset>
-              {/* Credentials */}
-              <FormFieldset legend={f('credentials')}>
-                <div className='grid w-full h-fit grid-cols-[1fr,1fr] gap-2'>
-                  <PhonePrefixInput name='prefix' isPending={isPending} />
-                  <PhoneNumberInput name='number' isPending={isPending} />
-                </div>
-                <EmailInput name='email' isPending={isPending} />
-                <PasswordInput name='password' isPending={isPending} />
-                <NewPasswordInput name='newPassword' isPending={isPending} />
-              </FormFieldset>
-              {/* Location */}
-              <FormFieldset legend={f('location')}>
-                <ZipCodeInput
-                  predictions={predictions}
-                  setValue={setValue}
-                  name='zipCode'
-                  isPending={isPending}
-                  setCountry={setCountry}
-                  setCity={setCity}
-                />
-                <CountryInput
-                  country={country}
-                  name='country'
-                  isPending={isPending}
-                />
-                <CityInput city={city} name='city' isPending={isPending} />
-                <AddressInput name='address' isPending={isPending} />
-              </FormFieldset>
-              {/* Vocation */}
-              <FormFieldset legend={f('vocation')}>
-                <OccupationInput name='occupation' isPending={isPending} />
-                <InterestsInput name='interests' isPending={isPending} />
-                <SloganInput name='slogan' isPending={isPending} />
-                <PortfolioInput name='portfolio' isPending={isPending} />
-              </FormFieldset>
+      <form className='space-y-8' onSubmit={onSubmit}>
+        <div className='space-y-8 mt-8'>
+
+          {/* IDENTITY */}
+          <FormFieldset legend={pf('identity')}>
+            <label htmlFor='name'>{f('inputs.name')}</label>
+            <input type='text' {...register('name')} />
+
+            <label htmlFor='nickname'>{f('inputs.nickname')}</label>
+            <input type='text' {...register('nickname')} />
+
+            <label htmlFor='gender'>{f('inputs.gender')}</label>
+            <input type='text' {...register('gender')} />
+
+            <label htmlFor='birthdate'>{f('inputs.birthdate')}</label>
+            <input type='text' {...register('birthdate')} />
+          </FormFieldset>
+
+          {/* CREDENTIALS */}
+          <FormFieldset legend={pf('credentials')}>
+            <div className='grid w-full h-fit grid-cols-[1fr,1fr] gap-2'>
+              <label htmlFor='phonePrefix'>{f('inputs.phonePrefix')}</label>
+              <input type='text' {...register('phonePrefix')} />
+
+              <label htmlFor='phoneNumber'>{f('inputs.phoneNumber')}</label>
+              <input type='text' {...register('phoneNumber')} />
             </div>
-            <FormError message={error} />
-            <FormSuccess message={success} />
-            <div className='w-full h-fit flex justify-end'>
-              <SubmitButton message={t('save')} isPending={isPending} />
-            </div>
-          </form>
-        </Form>
-      </FormProvider>
-    </CardWrapper>
-  ) : null
+
+            <label htmlFor='email'>{f('inputs.email')}</label>
+            <input type='text' {...register('email')} />
+
+            <label htmlFor='password'>{f('inputs.password')}</label>
+            <input type='text' {...register('password')} />
+
+            <label htmlFor='newPassword'>{f('inputs.newPassword')}</label>
+            <input type='text' {...register('newPassword')} />
+          </FormFieldset>
+
+          {/* LOCATION */}
+          <FormFieldset legend={pf('location')}>
+            <label htmlFor='zipCode'>{f('inputs.zipCode')}</label>
+            <input type='text' {...register('zipCode')} />
+
+            <label htmlFor='country'>{f('inputs.country')}</label>
+            <input type='text' {...register('country')} />
+
+            <label htmlFor='city'>{f('inputs.city')}</label>
+            <input type='text' {...register('city')} />
+
+            <label htmlFor='address'>{f('inputs.address')}</label>
+            <input type='text' {...register('address')} />
+          </FormFieldset>
+
+          {/* VOCATION */}
+          <FormFieldset legend={pf('location')}>
+            <label htmlFor='occupation'>{f('inputs.occupation')}</label>
+            <input type='text' {...register('occupation')} />
+
+            <label htmlFor='interests'>{f('inputs.interests')}</label>
+            <input type='text' {...register('interests')} />
+
+            <label htmlFor='slogan'>{f('inputs.slogan')}</label>
+            <input type='text' {...register('slogan')} />
+
+            <label htmlFor='portfolio'>{f('inputs.portfolio')}</label>
+            <input type='text' {...register('portfolio')} />
+          </FormFieldset>
+        </div>
+
+        {/* SUBMIT */}
+        <button type='submit'>Submit</button>
+      </form >
+    </CardWrapper >
+  )
 }
