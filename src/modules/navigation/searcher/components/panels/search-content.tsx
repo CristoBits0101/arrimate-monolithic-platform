867a3a8d2080a8082485db831ceda7ca61cde173
Cristo Suárez <cristobits0101@gmail.com>
2025-08-09 00:40:18 +0100

    Update text

--- src/modules/navigation/searcher/components/panels/search-content.tsx
+++ src/modules/navigation/searcher/components/panels/search-content.tsx
@@ -2,24 +2,23 @@
 import RecentContent from '@/modules/navigation/searcher/components/results/search-recent'
 import RecommendedContent from '@/modules/navigation/searcher/components/results/search-recommended'
 
+// Fonts
+import { ds } from '@/lib/google/google-fonts'
+
 // Hook
 import { useSearchContext } from '@/modules/navigation/searcher/hooks/useSearchContext'
 
 // Intl
-import { useTranslations } from 'next-intl'
 import SearchForm from '../forms/search-form'
 
 export default function SearchContent() {
   // Context
   const { searchTerm } = useSearchContext()
 
-  // Translations
-  const t = useTranslations('Searcher')
-
   return (
     <div className='w-full h-full py-8 grid grid-rows-[44px,1fr] gap-8'>
       <div className='w-full h-11 px-4 flex items-center'>
-        <h2 className='font-medium text-lg'>{t('placeholder')}</h2>
+        <h1 className={`${ds.className} text-4xl font-medium text-center w-full`}>Arrímate</h1>
       </div>
       <div className='w-full h-full flex flex-col gap-8'>
         <SearchForm />
