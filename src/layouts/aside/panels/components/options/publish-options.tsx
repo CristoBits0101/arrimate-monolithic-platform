804fdadfe1f0496f57de827761f35ee7d35d008a
Cristo Suárez <94800536+CristoBits0101@users.noreply.github.com>
2025-08-08 22:55:49 +0100

    feat: add dark mode icons for publications menu

--- src/layouts/aside/panels/components/options/publish-options.tsx
+++ src/layouts/aside/panels/components/options/publish-options.tsx
@@ -5,14 +5,22 @@ import OptionButton from '@/modules/configuration/settings-panel/buttons/option-
 
 // Custom
 import { usePost } from '@/modules/publications/add-post/hooks/usePost'
+import { useThemeContext } from '@/modules/configuration/settings-panel/hooks/useThemeContext'
 
 // Icons
+import audioDarkSVG from '@/assets/icons/navigation/inactive/dark-theme/streaming/audio-dark-icon.svg'
 import audioWhiteSVG from '@/assets/icons/navigation/inactive/light-theme/streaming/audio-light-icon.svg'
+import eventsDarkSVG from '@/assets/icons/navigation/inactive/dark-theme/feeds/events-dark-icon.svg'
 import eventsWhiteSVG from '@/assets/icons/navigation/inactive/light-theme/feeds/events-light-icon.svg'
+import productsDarkSVG from '@/assets/icons/navigation/inactive/dark-theme/e-commerce/products-dark-icon.svg'
 import productsWhiteSVG from '@/assets/icons/navigation/inactive/light-theme/e-commerce/products-light-icon.svg'
+import reviewsDarkSVG from '@/assets/icons/navigation/inactive/dark-theme/feeds/reviews-dark-icon.svg'
 import reviewsWhiteSVG from '@/assets/icons/navigation/inactive/light-theme/feeds/reviews-light-icon.svg'
+import shortsDarkSVG from '@/assets/icons/navigation/inactive/dark-theme/streaming/shorts-dark-icon.svg'
 import shortsWhiteSVG from '@/assets/icons/navigation/inactive/light-theme/streaming/shorts-light-icon.svg'
+import storyDarkSVG from '@/assets/icons/navigation/inactive/dark-theme/feeds/stories-dark-icon.svg'
 import storyWhiteSVG from '@/assets/icons/navigation/inactive/light-theme/feeds/stories-light-icon.svg'
+import videosDarkSVG from '@/assets/icons/navigation/inactive/dark-theme/streaming/videos-dark-icon.svg'
 import videosWhiteSVG from '@/assets/icons/navigation/inactive/light-theme/streaming/videos-light-icon.svg'
 
 // Intl
@@ -20,6 +28,7 @@ import { useTranslations } from 'next-intl'
 
 const PublishOptions = () => {
   const { changePost } = usePost()
+  const { activeTheme } = useThemeContext()
   const t = useTranslations('Posts')
 
   return (
@@ -28,49 +37,49 @@ const PublishOptions = () => {
         label={t('story')}
         isSelected={false}
         onClick={() => changePost('story')}
-        iconSrc={storyWhiteSVG}
+        iconSrc={activeTheme === 'light' ? storyWhiteSVG : storyDarkSVG}
         altText={t('story')}
       />
       <OptionButton
         label={t('event')}
         isSelected={false}
         onClick={() => changePost('event')}
-        iconSrc={eventsWhiteSVG}
+        iconSrc={activeTheme === 'light' ? eventsWhiteSVG : eventsDarkSVG}
         altText={t('event')}
       />
       <OptionButton
         label={t('product')}
         isSelected={false}
         onClick={() => changePost('product')}
-        iconSrc={productsWhiteSVG}
+        iconSrc={activeTheme === 'light' ? productsWhiteSVG : productsDarkSVG}
         altText={t('product')}
       />
       <OptionButton
         label={t('short')}
         isSelected={false}
         onClick={() => changePost('short')}
-        iconSrc={shortsWhiteSVG}
+        iconSrc={activeTheme === 'light' ? shortsWhiteSVG : shortsDarkSVG}
         altText={t('short')}
       />
       <OptionButton
         label={t('video')}
         isSelected={false}
         onClick={() => changePost('video')}
-        iconSrc={videosWhiteSVG}
+        iconSrc={activeTheme === 'light' ? videosWhiteSVG : videosDarkSVG}
         altText={t('video')}
       />
       <OptionButton
         label={t('audio')}
         isSelected={false}
         onClick={() => changePost('audio')}
-        iconSrc={audioWhiteSVG}
+        iconSrc={activeTheme === 'light' ? audioWhiteSVG : audioDarkSVG}
         altText={t('audio')}
       />
       <OptionButton
         label={t('review')}
         isSelected={false}
         onClick={() => changePost('review')}
-        iconSrc={reviewsWhiteSVG}
+        iconSrc={activeTheme === 'light' ? reviewsWhiteSVG : reviewsDarkSVG}
         altText={t('review')}
       />
     </section>
